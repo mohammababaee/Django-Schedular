5bb7566076dc1b3e6254d6e6594d9a50d19d9f9e
unknown <mohammadbabaeesem@gmail.com>
2024-04-16 08:06:53 +0430

    feat : add models for user and task

--- task/models.py
+++ task/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_to_send = models.DateTimeField(null=True, blank=True, default=None)
+    # Tasks that need to be completed before this task
+    pre_tasks = models.ManyToManyField('self', symmetrical=False, blank=True)
